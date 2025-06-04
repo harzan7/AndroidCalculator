d1d9db0d35b423042b2a0e149815bff8e930c285
Harza <harzan12345@gmail.com>
2025-06-04 19:23:05 -0400

    Add various colors that will be used in the app to Color.kt

--- app/src/main/java/com/harzan7/calculator/ui/theme/Color.kt
+++ app/src/main/java/com/harzan7/calculator/ui/theme/Color.kt
@@ -2,10 +2,21 @@ package com.harzan7.calculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light colors
+val LightRed = Color(0xFFB71C1C)
+val LightOrange = Color(0xFFE65100)
+val LightGreen = Color(0xFF1B5E20)
+val LightGray = Color(0xFF424242)
+val LightBackground = Color(0xFFF5F5F5)
+val LightOnBackground = Color(0xFF212121)
+val LightSurface = Color(0xFFFFFFFF)
+val LightOnSurface = Color(0xFF212121)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark colors
+val DarkRed = Color(0xFFEF5350)
+val DarkOrange = Color(0xFFFFA726)
+val DarkGreen = Color(0xFF66BB6A)
+val DarkGray = Color(0xFFB0BEC5)
+val DarkOnBackground = Color(0xFFE0E0E0)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkOnSurface = Color(0xFFE0E0E0)
