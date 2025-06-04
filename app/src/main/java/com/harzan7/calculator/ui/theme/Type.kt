f5485fe9ce006303963e844faf179198f7c53f9a
Harza <harzan12345@gmail.com>
2025-06-04 19:37:37 -0400

    Remove unused imports.

--- app/src/main/java/com/harzan7/calculator/ui/theme/Type.kt
+++ app/src/main/java/com/harzan7/calculator/ui/theme/Type.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import org.w3c.dom.Text
 
 val Typography = Typography(
     displayLarge = TextStyle(
