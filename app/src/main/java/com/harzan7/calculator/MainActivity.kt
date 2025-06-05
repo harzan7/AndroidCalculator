6dd8b5b506f55d1f848a4f57e0176c5099ee34de
Harza <harzan12345@gmail.com>
2025-06-05 17:03:42 -0400

    Remove starter code in MainActivity.kt.

--- app/src/main/java/com/harzan7/calculator/MainActivity.kt
+++ app/src/main/java/com/harzan7/calculator/MainActivity.kt
@@ -17,16 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            CalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContent { }
     }
 }
 
