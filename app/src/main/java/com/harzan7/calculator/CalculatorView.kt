07be93fdd44e3e0e091f62806bf13fd45a17ccb0
Harza <harzan12345@gmail.com>
2025-06-05 17:14:44 -0400

    Add base scaffold for Calculator UI.

--- app/src/main/java/com/harzan7/calculator/CalculatorView.kt
+++ app/src/main/java/com/harzan7/calculator/CalculatorView.kt
@@ -1,5 +1,13 @@
 package com.harzan7.calculator
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.harzan7.calculator.ui.theme.CalculatorTheme
+
 val buttonList = listOf(
     "C", "(", ")", "÷",
     "7", "8", "9", "×",
@@ -7,3 +15,33 @@ val buttonList = listOf(
     "1", "2", "3", "−",
     "AC", "0", "•", "="
 )
+
+@Composable
+fun CalculatorApp() {
+    CalculatorTheme {
+        CalculatorScreen()
+    }
+}
+
+@Composable
+fun CalculatorScreen(
+    // TODO: viewModel
+) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.background
+    ) {
+        innerPadding ->
+        CalculatorContent(
+            modifier = Modifier.padding(innerPadding),
+            // TODO: viewModel
+        )
+
+    }
+}
+
+@Composable
+fun CalculatorContent(
+    modifier: Modifier = Modifier,
+    // TODO: viewModel
+) {}
