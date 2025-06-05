d8758a5e6d750a45ec7ac3c0ec2df48e4f2df7d2
Harza <harzan12345@gmail.com>
2025-06-05 17:04:11 -0400

    Create layout for button pad in CalculatorView.kt.

--- app/src/main/java/com/harzan7/calculator/CalculatorView.kt
+++ app/src/main/java/com/harzan7/calculator/CalculatorView.kt
@@ -1,2 +1,9 @@
 package com.harzan7.calculator
 
+val buttonList = listOf(
+    "C", "(", ")", "÷",
+    "7", "8", "9", "×",
+    "4", "5", "6", "+",
+    "1", "2", "3", "−",
+    "AC", "0", "•", "="
+)
