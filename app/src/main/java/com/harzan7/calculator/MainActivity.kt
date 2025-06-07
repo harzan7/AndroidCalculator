400195ac29ff551a33da6dd9a5210d0112a48307
Harza <harzan12345@gmail.com>
2025-06-06 23:44:50 -0400

    Add Calculator View call to MainActivity, and cleaned up unnecessary imports.
    Everything works as expected.

--- app/src/main/java/com/harzan7/calculator/MainActivity.kt
+++ app/src/main/java/com/harzan7/calculator/MainActivity.kt
@@ -4,32 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.harzan7.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculatorTheme {
-        Greeting("Android")
+        setContent { CalculatorApp() }
     }
 }
